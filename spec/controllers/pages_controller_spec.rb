72f4d73f69b07ab80df732777623f842525e08bf
Tom Barron <tom.barron@comcast.net>
2011-12-09 21:06:01 -0500

    Passing tests for home, contact, about pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -23,7 +23,7 @@ describe PagesController do
     end
 
     it "should have the right title" do
-      get 'home'
+      get 'contact'
       response.should have_selector("title",
                                     :content => "Learning RoR | Contact")
     end
@@ -36,7 +36,7 @@ describe PagesController do
     end
 
     it "should have the right title" do
-      get 'home'
+      get 'about'
       response.should have_selector("title",
                                     :content => "Learning RoR | About")
     end
