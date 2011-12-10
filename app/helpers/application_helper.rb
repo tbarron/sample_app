4d68bebf8c190cfc2cfc5fb04627c007553b484a
Tom Barron <tom.barron@comcast.net>
2011-12-10 08:43:38 -0500

    Added helper to manage title across pages. Autotest in Gemfile broke heroku deployment -- removed.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Return a page-specific title if @title defined, else generic title
+  def title
+    base_title = "Learning Ruby on Rails"
+    if @title.nil?
+      return base_title
+    else
+      return "#{base_title} | #{@title}"
+    end
+  end
+
 end
