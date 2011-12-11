7cd554498045e461eb8b4b1c0280b91f25b61866
Tom Barron <tom.barron@comcast.net>
2011-12-10 21:10:47 -0500

    Add registration page, tests, and support

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -21,4 +21,9 @@ describe "LayoutLinks" do
     get '/help'
     response.should have_selector('title', :content => "Help")
   end
+
+  it "should have a registration page at '/register'" do
+    get '/register'
+    response.should have_selector('title', :content => "Register")
+  end
 end
