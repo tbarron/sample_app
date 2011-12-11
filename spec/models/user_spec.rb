f83c07336e6cef35d0284229c01e7b0bb815a7cd
Tom Barron <tom.barron@comcast.net>
2011-12-11 12:43:41 -0500

    Add tested validations for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,27 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = { :name => "Example User", :email => "user@example.com" }
+  end
+
+  it "should create a new instance given valid attributes" do
+    User.create!(@attr)
+  end
+
+  it "should require a name" do
+    no_name = User.new(@attr.merge(:name => ""))
+    no_name.should_not be_valid
+  end
+
+  it "should require an email address" do
+    no_mail = User.new(@attr.merge(:email => ""))
+    no_mail.should_not be_valid
+  end
+
+  it "should limit names to maximum length" do
+    long_name = "a" * 51
+    too_long = User.new(@attr.merge(:name => long_name))
+    too_long.should_not be_valid
+  end
 end
