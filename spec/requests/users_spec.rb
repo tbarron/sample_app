24236f4605b28f65da0e6938ef78c10ae10cc39f
Tom Barron <tom.barron@comcast.net>
2011-12-16 19:00:36 -0500

    Integration test for successful registration request

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -16,5 +16,21 @@ describe "Users" do
         end.should_not change(User, :count)
       end
     end
+
+    describe "success" do
+      it "should make a new user" do
+        lambda do
+          visit register_path
+          fill_in "Name", :with => "Example User"
+          fill_in "Email", :with => "user@example.com"
+          fill_in "Password", :with => "foobar"
+          fill_in "Password confirmation", :with => "foobar"
+          click_button "Register"
+          response.should have_selector("div.flash.success",
+                                        :content => "Welcome")
+          response.should render_template('users/show')
+        end.should change(User, :count).by(1)
+      end
+    end
   end
 end
