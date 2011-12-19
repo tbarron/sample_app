24db12ceb4704f8b525167b72adc6ca76f681046
Tom Barron <tom.barron@comcast.net>
2011-12-18 21:14:46 -0500

    Stuff for editing users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,4 +22,9 @@ class UsersController < ApplicationController
       render 'new'  # !@! would :new work here?
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
 end
